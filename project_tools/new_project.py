ccf7b3a7131977ad9309d73fd2f1ce979828a163
UnsignedArduino <UnsignedArduino@outlook.com>
2021-03-25 10:04:14 -0400

    Can actually make a new project

--- project_tools/new_project.py
+++ project_tools/new_project.py
@@ -11,15 +11,29 @@ No classes!
 
 Functions list:
 
-No functions!
+- replace_sus_chars(file_name: str) -> str
+- make_new_project(parent_directory: Path, project_name: str = "Untitled", autogen_gitignore: bool = True,
+                   fl_cpy_hierarchy: Path = (Path.cwd() / "default_circuitpython_hierarchy")) -> None
 
 """
 
 from pathlib import Path
+import shutil
+import re
 from project_tools.create_logger import create_logger
 import logging
 
 logger = create_logger(name=__name__, level=logging.DEBUG)
+
+
+def replace_sus_chars(file_name: str) -> str:
+    """
+    Replace suspicious characters in file name - found at https://stackoverflow.com/a/13593932/10291933
+
+    :param file_name: A str - the file name to check.
+    :return: A str - the file name cleaned.
+    """
+    return re.sub("[^\w\-_. ]", "_", file_name)
 
 
 def make_new_project(parent_directory: Path, project_name: str = "Untitled", autogen_gitignore: bool = True,
@@ -34,3 +48,9 @@ def make_new_project(parent_directory: Path, project_name: str = "Untitled", aut
      Path.cwd() / "default_circuitpython_hierarchy"
     :return: None.
     """
+    project_path = parent_directory / dfl_cpy_hierarchy.name
+    logger.debug(f"Copying from {repr(dfl_cpy_hierarchy)} to {repr(project_path)}")
+    shutil.copytree(dfl_cpy_hierarchy, project_path)
+    new_path = parent_directory / replace_sus_chars(project_name)
+    logger.debug(f"Renaming {repr(project_path)} to {repr(new_path)}")
+    project_path.rename(new_path)
